1e8a9749ccff63de902556b03a0a7c35e52256a1
Aqsa Naseer <arzoosultani123456@gmail.com>
2024-03-16 23:48:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Task no. 2
+let Name = "Aqsa";
+console.log(`Hello ${Name}, would you like to learn some paython?`);
+// Task no3
+let Name = "Aqsa";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Task no. 2
+let Name1 : string = "Aqsa";
+console.log(`Hello ${Name1}, would you like to learn some paython?`);
+
+// Task no3
+let Name: string = "Aqsa";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase())
