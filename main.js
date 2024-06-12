2cc316621448506af3b7ed7a62b0a5e80c604cfd
Aqsa Naseer <163171532+aqsanaseer11@users.noreply.github.com>
2024-06-12 20:36:12 +0500

    add new method for print name into titlecase

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 "use strict";
 // Task no. 2
-let Name = "Aqsa";
-console.log(`Hello ${Name}, would you like to learn some paython?`);
+let Name1 = "Aqsa";
+console.log(`Hello ${Name1}, would you like to learn some paython?`);
 // Task no3
 let Name = "Aqsa";
 console.log(Name.toLowerCase());
 console.log(Name.toUpperCase());
-console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
+//console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase())
+console.log(Name.replace(/\b\w/g, (abs) => abs.toUpperCase()));
--- main.ts
+++ main.ts
@@ -7,3 +7,4 @@ let Name: string = "Aqsa";
 console.log(Name.toLowerCase());
 console.log(Name.toUpperCase());
 console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase())
+//console.log(Name.replace(/\b\w/g, (abs) => abs.toUpperCase()));
